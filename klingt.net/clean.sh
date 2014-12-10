1b4f894fb2e3b7eebcfdcc431666f7e7706aaacb
Andreas Linz <klingt.net@gmail.com>
2014-12-10 10:37:17 +0100

    changed quotes because variables were not expanded

--- klingt.net/clean.sh
+++ klingt.net/clean.sh
@@ -9,21 +9,21 @@ fi
 
 nikola clean --clean-all
 if [ -d $THEME/sass/.sass-cache ]; then
-	echo 'removing .sass-cache'
+	echo "removing .sass-cache"
 	rm -r $THEME/sass/.sass-cache
 fi
 for c in "${CACHE[@]}"; do
 	if [ -d $c ]; then
-		echo 'removing nikola cache dir $CACHE'
+		echo "removing nikola cache dir $CACHE"
 		rm -r $c
 	fi
 done
 if [ -f .doit.db ]; then
-	echo 'removing doit.db'
+	echo "removing doit.db"
 	rm .doit.db
 fi
 if [ -d output ]; then
-	echo 'removing output directory!'
+	echo "removing output directory!"
 	rm -r ./output
 fi
 
