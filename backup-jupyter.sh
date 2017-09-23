d707a5ce311ceb1a2e213d4a0f25d39267374002
Andreas Linz <klingt.net@gmail.com>
2017-09-23 17:55:52 +0200

    Backup Julia data

--- backup-jupyter.sh
+++ backup-jupyter.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 timestamp=$(date --iso-8601=seconds)
 
 ssh klingt.vnet "sudo systemctl kill jupyter"
-ssh klingt.vnet "sudo tar -C /home/jupyter -cf - -- notebooks | pixz" > "backups/jupyter-files-${timestamp}.tar.xz"
+ssh klingt.vnet "sudo tar -C /home/jupyter -cf - -- notebooks .julia | pixz" > "backups/jupyter-files-${timestamp}.tar.xz"
 ssh klingt.vnet "sudo systemctl start jupyter"
