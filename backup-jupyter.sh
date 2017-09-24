64e1963e3b87635420550692149e98742e4c3f0e
Andreas Linz <klingt.net@gmail.com>
2017-09-24 23:21:15 +0200

    Merge backup and restore scripts

--- backup-jupyter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-timestamp=$(date --iso-8601=seconds)
-
-ssh klingt.vnet "sudo systemctl kill jupyter"
-ssh klingt.vnet "sudo tar -C /home/jupyter -cf - -- notebooks .julia | pixz" > "backups/jupyter-files-${timestamp}.tar.xz"
-ssh klingt.vnet "sudo systemctl start jupyter"
---
+++ backup.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+selection=${1:-''}
+set -euo pipefail
+
+[[ ! -e './backups' ]] && mkdir backups
+
+timestamp=$(date --iso-8601=seconds)
+domain=klingt.vnet
+
+_psql() {
+    local database="$1"
+    local fname="backups/${database}-psql-${timestamp}.xz"
+    echo "Creating $fname ..."
+    ssh ${domain} "pg_dump --user=alinz $database --format=custom | pixz" > "$fname"
+}
+
+_caddy() {
+    ssh ${domain} "sudo systemctl stop caddy"
+    ssh ${domain} "sudo tar -C /home/caddy -cf - -- certs restic files.${domain} | pixz" > "backups/caddy-files-${timestamp}.tar.xz"
+    ssh ${domain} "sudo systemctl start caddy"
+}
+
+_gitea() {
+    ssh ${domain} "sudo systemctl stop gitea"
+    ssh ${domain} "sudo tar -C /home/gitea -cf - -- gitea | pixz" > "backups/gitea-files-${timestamp}.tar.xz"
+    _psql gitea $timestamp
+    ssh ${domain} "sudo systemctl start gitea"
+}
+
+_grafana() {
+    ssh ${domain} "sudo systemctl stop grafana"
+    _psql grafana $timestamp
+    ssh ${domain} "sudo systemctl start grafana"
+}
+
+_jupyter() {
+    ssh ${domain} "sudo systemctl kill jupyter"
+    ssh ${domain} "sudo tar -C /home/jupyter -cf - -- notebooks .julia | pixz" > "backups/jupyter-files-${timestamp}.tar.xz"
+    ssh ${domain} "sudo systemctl start jupyter"
+}
+
+_prometheus() {
+    ssh ${domain} "sudo systemctl kill prometheus"
+    ssh ${domain} "sudo tar -C /home/prometheus -cf - -- data | pixz" > "backups/prometheus-files-${timestamp}.tar.xz"
+    ssh ${domain} "sudo systemctl start prometheus"
+}
+
+case "$selection" in
+    'caddy')
+        _caddy
+        ;;
+    'gitea')
+        _gitea
+        ;;
+    'grafana')
+        _grafana
+        ;;
+    'jupyter')
+        _jupyter
+        ;;
+    'prometheus')
+        _prometheus
+        ;;
+    ''|'all')
+        _caddy
+        _gitea
+        _grafana
+        _jupyter
+        _prometheus
+        ;;
+esac
