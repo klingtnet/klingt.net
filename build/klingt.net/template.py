dacc78add469a1c5e8535428b034b2d4502f48fa
Andreas Linz <klingt.net@gmail.com>
2018-05-26 17:41:26 +0200

    Write a simple GDPR compliant website with tiny templating

---
+++ build/klingt.net/template.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+
+def read_template():
+    with open('template.html') as f:
+        return f.read()
+
+
+def title_from_filename(fname: str) -> str:
+    return os.path.splitext(fname)[0].capitalize()
+
+
+def template_pages(pages: dict, template: str, dest_path: str):
+    nav_item = '<li><a href="{item_href}">{item_text}</a></li>'
+    
+    for page, text in pages.items():
+        title = title_from_filename(page)
+        other_pages = [ p for p in pages.keys() if p != page ]
+        nav = ''.join([ nav_item.format(item_href=p, item_text=title_from_filename(p)) for p in other_pages ])
+        templated_page = template.format(title=title, nav=nav, content=text)
+        
+        with open(os.path.join(dest_path, page), 'w') as f:
+            f.write(templated_page)
+
+
+pages = dict()
+for page in os.listdir('pages'):
+    with open(os.path.join('pages', page)) as f:
+        pages[page] = f.read()
+
+template_pages(pages, read_template(), 'out')
